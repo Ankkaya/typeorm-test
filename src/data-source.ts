6834efdf4886140db800c74557062dee59193dc7
Ankkaya <staryou@126.com>
2024-01-19 10:16:31 +0800

    44

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata'
 import { DataSource } from 'typeorm'
 import { User } from './entity/User'
+import { Aaa } from './entity/Aaa'
 
 export const AppDataSource = new DataSource({
     type: 'mysql',
@@ -11,9 +12,10 @@ export const AppDataSource = new DataSource({
     database: 'practice',
     synchronize: true,
     logging: true,
-    entities: [User],
+    entities: [User, Aaa],
     migrations: [],
     subscribers: [],
+    poolSize: 10,
     connectorPackage: 'mysql2',
     extra: {
         authPlugin: 'sha256_password',
